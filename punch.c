b1b6604e889fc1eb37c472e9fc9c7ad7cac3c044
Will Fleming <will@flemi.ng>
2016-03-05 16:29:55 -0500

    sketch out subcommand parsing & exec

--- punch.c
+++ punch.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 
-/*int main(int argc, char **argv) {*/
-int main() {
-  puts("Hello World");
+/*---------- Prototypes ----------*/
+
+void help_cmd(int argc, char **argv);
+void in_cmd(int argc, char **argv);
+void out_cmd(int argc, char **argv);
+void summary_cmd(int argc, char **argv);
+
+/*---------- Implementations ----------*/
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    help_cmd(0, NULL);
+  } else if (0 == strncmp("in", argv[1], 3)) {
+    in_cmd(argc - 2, argv + 2);
+  } else if (0 == strncmp("out", argv[1], 4)) {
+    out_cmd(argc - 2, argv + 2);
+  } else if (0 == strncmp("summary", argv[1], 8)) {
+    summary_cmd(argc - 2, argv + 2);
+  } else {
+    help_cmd(argc - 2, argv + 2);
+  }
   return 0;
+}
+
+void help_cmd(int argc, char **argv) {
+  (void) argc; (void) argv; // not used yet
+  puts("Punch is a simple time tracker.\n\n" \
+       "Usage:\n\n" \
+       "\tpunch <command> [arguments]\n\n" \
+       "The commands are:\n\n" \
+       "\tin\tpunch in to a project\n" \
+       "\tout\tpunch out of a project\n" \
+       "\tsummary\tsummarize your time");
+}
+
+void in_cmd(int argc, char **argv) {
+  (void) argc; (void) argv; // not used yet
+  puts("TODO: implement in command.");
+}
+
+void out_cmd(int argc, char **argv) {
+  (void) argc; (void) argv; // not used yet
+  puts("TODO: implement out command.");
+}
+
+void summary_cmd(int argc, char **argv) {
+  (void) argc; (void) argv; // not used yet
+  puts("TODO: implement summary command.");
 }
