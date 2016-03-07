c0c53793487e4b116e6c30368e60b1f0e3b834c9
Will Fleming <will@flemi.ng>
2016-03-06 23:55:06 -0500

    main: complete help usage
    * sub-command docs
    * allow running either `punch help <command>` or `punch <command>
      --help`

--- punch.c
+++ punch.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include "entry_log.h"
@@ -5,49 +6,75 @@
 /*---------- Prototypes ----------*/
 
 void help_cmd(int argc, char **argv);
-void in_cmd(int argc, char **argv);
-void out_cmd(int argc, char **argv);
-void summary_cmd(int argc, char **argv);
+int in_cmd(int argc, char **argv);
+int out_cmd(int argc, char **argv);
+int summary_cmd(int argc, char **argv);
+bool show_cmd_help(char *cmd, int argc, char **argv);
 
 /*---------- Implementations ----------*/
 
 int main(int argc, char **argv) {
+  int status = 0;
   if (argc < 2) {
     help_cmd(0, NULL);
   } else if (0 == strncmp("in", argv[1], 3)) {
-    in_cmd(argc - 2, argv + 2);
+    status = in_cmd(argc - 2, argv + 2);
   } else if (0 == strncmp("out", argv[1], 4)) {
-    out_cmd(argc - 2, argv + 2);
+    status = out_cmd(argc - 2, argv + 2);
   } else if (0 == strncmp("summary", argv[1], 8)) {
-    summary_cmd(argc - 2, argv + 2);
+    status = summary_cmd(argc - 2, argv + 2);
   } else {
     help_cmd(argc - 2, argv + 2);
   }
-  return 0;
+  return status;
 }
 
 void help_cmd(int argc, char **argv) {
-  (void) argc; (void) argv; // not used yet
-  puts("Punch is a simple time tracker.\n\n" \
-       "Usage:\n\n" \
-       "\tpunch <command> [arguments]\n\n" \
-       "The commands are:\n\n" \
-       "\tin\tpunch in to a project\n" \
-       "\tout\tpunch out of a project\n" \
-       "\tsummary\tsummarize your time");
+  if (argc > 0 && 0 == strcmp(argv[0], "in")) {
+    puts("usage: punch in <project name>");
+  } else if (argc > 0 && 0 == strcmp(argv[0], "out")) {
+    puts("usage: punch out");
+  } else if (argc > 0 && 0 == strcmp(argv[0], "summary")) {
+    puts("usage: punch summary [--csv]");
+  } else {
+    puts("Punch is a simple time tracker.\n\n" \
+         "Usage:\n\n" \
+         "\tpunch <command> [arguments]\n\n" \
+         "The commands are:\n\n" \
+         "\tin\tpunch in to a project\n" \
+         "\tout\tpunch out of a project\n" \
+         "\tsummary\tsummarize your time");
+  }
 }
 
-void in_cmd(int argc, char **argv) {
-  (void) argc; (void) argv; // not used yet
+int in_cmd(int argc, char **argv) {
+  if (show_cmd_help("in", argc, argv)) {
+    return 0;
+  }
   puts("TODO: implement in command.");
+  return 0;
 }
 
-void out_cmd(int argc, char **argv) {
-  (void) argc; (void) argv; // not used yet
+int out_cmd(int argc, char **argv) {
+  if (show_cmd_help("out", argc, argv)) {
+    return 0;
+  }
   puts("TODO: implement out command.");
+  return 0;
 }
 
-void summary_cmd(int argc, char **argv) {
-  (void) argc; (void) argv; // not used yet
+int summary_cmd(int argc, char **argv) {
+  if (show_cmd_help("summary", argc, argv)) {
+    return 0;
+  }
   puts("TODO: implement summary command.");
+  return 0;
+}
+
+bool show_cmd_help(char *cmd, int argc, char **argv) {
+  if (argc > 0 && (0 == strcmp(argv[0], "-h") || 0 == strcmp(argv[0], "--help"))) {
+    help_cmd(1, &cmd);
+    return true;
+  }
+  return false;
 }
