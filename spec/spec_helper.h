8558ae2d15a2a96efbcd7999a753cc0e93d24f51
Will Fleming <will@flemi.ng>
2018-08-07 23:13:27 -0400

    Add time equality test to spec helper

--- spec/spec_helper.h
+++ spec/spec_helper.h
@@ -2,6 +2,7 @@
 #include <speck.h>
 #include <time.h>
 #include "entry.h"
+#include "iso8601.h"
 
 #define SET_TZ(tz) char *_old_tz = getenv("TZ"); setenv("TZ", tz, 1); tzset();
 #define RESET_TZ if (NULL == _old_tz) { unsetenv("TZ"); } else { setenv("TZ", _old_tz, 1); } tzset();
@@ -44,6 +45,34 @@ void sp_assert_equal_d_lineno(double x, double y, int lineno)
     state.codes = realloc(state.codes, (state.index + 1) * sizeof(int));
 
     if (x == y) {
+        state.codes[state.index] = 0;
+    } else {
+        state.codes[state.index] = 1;
+    }
+
+    state.index++;
+}
+
+#define sp_assert_equal_tm(x, y) sp_assert_equal_tm_lineno(x, y, __LINE__)
+
+void sp_assert_equal_tm_lineno(struct tm x, struct tm y, int lineno) {
+    state.assertions = realloc(state.assertions, (state.index + 1) * sizeof(char *));
+    char x_str[ISO8601_STR_LEN], y_str[ISO8601_STR_LEN];
+    strfiso8601(x_str, ISO8601_STR_LEN, &x);
+    strfiso8601(y_str, ISO8601_STR_LEN, &y);
+    alloc_sprintf(&(state.assertions[state.index]), ":%d -> %s::sp_assert_equal_time_t(%s, %s)", lineno, state.function, x_str, y_str);
+
+    state.codes = realloc(state.codes, (state.index + 1) * sizeof(int));
+
+    if (x.tm_year == y.tm_year &&
+        x.tm_mon == y.tm_mon &&
+        x.tm_yday == y.tm_yday &&
+        x.tm_wday == y.tm_wday &&
+        x.tm_mday == y.tm_mday &&
+        x.tm_hour == y.tm_hour &&
+        x.tm_min == y.tm_min &&
+        x.tm_sec == y.tm_sec
+        ) {
         state.codes[state.index] = 0;
     } else {
         state.codes[state.index] = 1;
